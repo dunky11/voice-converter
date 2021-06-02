e0c36fc2c112ef05b279ddba38453d977d5df4b0
dunky11 <timv94@zedat.fu-berlin.de>
2021-06-02 14:52:48 +0200

    Initial commit

---
+++ android/app/src/main/java/com/voiceconversion/CalendarModule.java
@@ -0,0 +1,25 @@
+package com.voiceconversion;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import java.util.Map;
+import java.util.HashMap;
+
+public class CalendarModule extends ReactContextBaseJavaModule {
+    CalendarModule(ReactApplicationContext context) {
+        super(context);
+    }
+
+    // add to CalendarModule.java
+    @Override
+    public String getName() {
+        return "CalendarModule";
+    }
+
+    @ReactMethod
+    public String createCalendarEvent(String name, String location) {
+        return "Hello world!";
+    }
+}
---
+++ pages/ConvertAudio.js
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+    StyleSheet, 
+    View,
+    Image,
+    Dimensions
+} from  "react-native";
+import {
+    withTheme,
+    ProgressBar,
+    Title,
+    IconButton,
+    Headline,
+} from "react-native-paper";
+
+function UnlockPremium(props) {
+    const {theme, onCancelLoading, progress} = props;
+    return (
+        <View style={styles.container}>
+            <View>
+                <View style={{position: "relative"}}>
+                    <Image 
+                        source={require('../assets/imgs/trump_512x512.png')}
+                        style={{
+                            width: Dimensions.get('window').width - 32, 
+                            height: Dimensions.get('window').width - 32,
+                            marginBottom: 16,
+                            borderRadius: theme.roundness
+                        }} 
+                    />
+                    <View style={{
+                        position: "absolute", 
+                        right:0, 
+                        borderTopRightRadius: theme.roundness,
+                        borderBottomRightRadius: theme.roundness,  
+                        backgroundColor: "black",
+                        opacity: 0.8, 
+                        height: Dimensions.get('window').width - 32, 
+                        width: (1.0 - progress) * Dimensions.get('window').width - 32
+                    }}/>
+                </View>
+                <Headline 
+                    style={{marginTop: 8, textAlign: "center"}}
+                >
+                        {progress * 100}%
+                </Headline>
+            </View>
+            <View style={{alignItems: "center"}}>
+                <IconButton 
+                    icon="close-circle"
+                    size={80}
+                    onPress={onCancelLoading}
+                />
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: { 
+        backgroundColor: "#121212",
+        paddingTop: 16,
+        paddingBottom: 16,
+        paddingLeft: 16,
+        paddingRight: 16,
+        flex: 1,
+        display: "flex",
+        justifyContent: "space-between"
+    },
+});
+
+export default withTheme(UnlockPremium);
