4bf73de46931abd2aa3f70c811bf422a4ca74f27
dunky11 <timv94@zedat.fu-berlin.de>
2021-06-03 10:37:45 +0200

    Backing up before dropping tfmobile

--- android/app/src/main/java/com/voiceconversion/CalendarModule.java
+++ android/app/src/main/java/com/voiceconversion/CalendarModule.java
@@ -7,9 +7,18 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import android.content.res.AssetManager;
 import android.content.Context;
+import android.util.Log;
+import java.nio.FloatBuffer;
+
 import org.pytorch.Tensor;
 import org.pytorch.Module;
 import org.pytorch.IValue;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class CalendarModule extends ReactContextBaseJavaModule {
     Context context;
@@ -25,11 +34,47 @@ public class CalendarModule extends ReactContextBaseJavaModule {
         return "CalendarModule";
     }
 
-    @ReactMethod
-    public void createCalendarEvent(Callback cb) {
-        AssetManager assetManager = this.context.getAssets();
-        Module module = Module.load(assetManager+"/models/voice_converter.pt");
+    public static String assetFilePath(Context context, String assetName) {
+        File file = new File(context.getFilesDir(), assetName);
+        if (file.exists() && file.length() > 0) {
+            return file.getAbsolutePath();
+        }
 
+        try (InputStream is = context.getAssets().open(assetName)) {
+            try (OutputStream os = new FileOutputStream(file)) {
+                byte[] buffer = new byte[4 * 1024];
+                int read;
+                while ((read = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, read);
+                }
+                os.flush();
+            }
+            return file.getAbsolutePath();
+        } catch (IOException e) {
+            System.out.println("Error process asset " + assetName + " to file path");
+        }
+        return null;
+    }
+
+    @ReactMethod
+    public void createCalendarEvent(Callback cb) throws IOException {
+        AssetManager assetManager = this.context.getAssets();
+        String[] fileNames = assetManager.list("models");
+        for(String name:fileNames){
+            System.out.println("Folder path " + name);
+        }
+        Module model = Module.load(assetFilePath(this.context, "model.pt"));
+        float[] input  = new float[10 * 80 * 88];
+        for(int i = 0; i < input.length; i++) {
+            input[i] = 1.0f;
+        }
+        Tensor data =
+                Tensor.fromBlob(
+                        input,
+                        new long[] {10, 80, 88}
+                );
+        IValue result = model.forward(IValue.from(data));
+        Tensor output = result.toTensor();
         cb.invoke(assetManager+"/books/IndividualVillas/images-bali/1.jpg");
     }
 }
