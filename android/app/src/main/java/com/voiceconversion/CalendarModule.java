c6d7d0993e2502f41f4effa51bb76bf50ac801ad
dunky11 <timv94@zedat.fu-berlin.de>
2021-06-04 01:58:42 +0200

    Model importation works for mel2mel

--- android/app/src/main/java/com/voiceconversion/CalendarModule.java
+++ android/app/src/main/java/com/voiceconversion/CalendarModule.java
@@ -5,14 +5,22 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.voiceconversion.ml.MelToMel;
+import com.voiceconversion.ml.MelToWav;
+
 import android.content.res.AssetManager;
 import android.content.Context;
 import android.util.Log;
-import java.nio.FloatBuffer;
 
-import org.pytorch.Tensor;
-import org.pytorch.Module;
-import org.pytorch.IValue;
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.gpu.CompatibilityList;
+import org.tensorflow.lite.gpu.GpuDelegate;
+
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -34,47 +42,29 @@ public class CalendarModule extends ReactContextBaseJavaModule {
         return "CalendarModule";
     }
 
-    public static String assetFilePath(Context context, String assetName) {
-        File file = new File(context.getFilesDir(), assetName);
-        if (file.exists() && file.length() > 0) {
-            return file.getAbsolutePath();
-        }
-
-        try (InputStream is = context.getAssets().open(assetName)) {
-            try (OutputStream os = new FileOutputStream(file)) {
-                byte[] buffer = new byte[4 * 1024];
-                int read;
-                while ((read = is.read(buffer)) != -1) {
-                    os.write(buffer, 0, read);
-                }
-                os.flush();
-            }
-            return file.getAbsolutePath();
-        } catch (IOException e) {
-            System.out.println("Error process asset " + assetName + " to file path");
-        }
-        return null;
-    }
-
     @ReactMethod
     public void createCalendarEvent(Callback cb) throws IOException {
-        AssetManager assetManager = this.context.getAssets();
-        String[] fileNames = assetManager.list("models");
-        for(String name:fileNames){
-            System.out.println("Folder path " + name);
+
+        // Initialize interpreter with GPU delegate
+        Interpreter.Options options = new Interpreter.Options();
+        CompatibilityList compatList = new CompatibilityList();
+
+        if(compatList.isDelegateSupportedOnThisDevice()){
+            // if the device has a supported GPU, add the GPU delegate
+            GpuDelegate.Options delegateOptions = compatList.getBestOptionsForThisDevice();
+            GpuDelegate gpuDelegate = new GpuDelegate(delegateOptions);
+            options.addDelegate(gpuDelegate);
+        } else {
+            // if the GPU is not supported, run on 4 threads
+            options.setNumThreads(4);
         }
-        Module model = Module.load(assetFilePath(this.context, "model.pt"));
-        float[] input  = new float[10 * 80 * 88];
-        for(int i = 0; i < input.length; i++) {
-            input[i] = 1.0f;
-        }
-        Tensor data =
-                Tensor.fromBlob(
-                        input,
-                        new long[] {10, 80, 88}
-                );
-        IValue result = model.forward(IValue.from(data));
-        Tensor output = result.toTensor();
-        cb.invoke(assetManager+"/books/IndividualVillas/images-bali/1.jpg");
+        MelToMel model = MelToMel.newInstance(this.context);
+        TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 80, 80}, DataType.FLOAT32);
+
+        MelToWav model2 = MelToWav.newInstance(this.context);
+
+        System.out.println("HERE");
+
+        cb.invoke("/books/IndividualVillas/images-bali/1.jpg");
     }
 }
